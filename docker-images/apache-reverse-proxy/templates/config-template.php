a12e874fe504c4c30b824b1786f25e9a49a2e9f1
Elie Kin <elie.ndjoli@outlook.com>
2018-05-28 23:02:50 +0200

    Load balancing fonctionnel

--- docker-images/apache-reverse-proxy/templates/config-template.php
+++ docker-images/apache-reverse-proxy/templates/config-template.php
@@ -1,16 +1,37 @@
 <?php
-	$static_app = getenv('STATIC_APP');
-	$dynamic_app = getenv('DYNAMIC_APP');
+	$static_app1 = getenv('STATIC_APP1');
+	$static_app2 = getenv('STATIC_APP2');
+	$static_app3 = getenv('STATIC_APP3');
+	$dynamic_app1 = getenv('DYNAMIC_APP1');
+	$dynamic_app2 = getenv('DYNAMIC_APP2');
+	$dynamic_app3 = getenv('DYNAMIC_APP3');
 ?>
 <VirtualHost *:80>
         ServerName demo.res.ch
 
         #ErrorLog ${APACHE_LOG_DIR}/error.log
         #CustomLog ${APACHE_LOG_DIR}/access.log combined
+		
+		<Proxy balancer://mysetdynamic>
+			BalancerMember 'http://<?php print "$dynamic_app1"?>'
+			BalancerMember 'http://<?php print "$dynamic_app2"?>'
+			BalancerMember 'http://<?php print "$dynamic_app3"?>'
+		</Proxy>
+		
+		<Proxy balancer://mysetstatic>
+			BalancerMember 'http://<?php print "$static_app1"?>'
+			BalancerMember 'http://<?php print "$static_app2"?>'
+			BalancerMember 'http://<?php print "$static_app3"?>'
+		</Proxy>
 
-        ProxyPass '/api/students/' 'http://<?php print "$dynamic_app"?>/'
-        ProxyPassReverse '/api/students/' 'http://<?php print "$dynamic_app"?>/'
+        ProxyPass '/api/students/' 'balancer://mysetdynamic/'
+        ProxyPassReverse '/api/students/' 'balancer://mysetdynamic/'
 
-        ProxyPass '/' 'http://<?php print "$static_app"?>/'
-        ProxyPassReverse '/' 'http://<?php print "$static_app"?>/'
+        ProxyPass '/' 'balancer://mysetstatic/'
+        ProxyPassReverse '/' 'balancer://mysetstatic/'
+		
+		<Location "/balancer-manager">
+			SetHandler balancer-manager
+			Require host demo.res.ch
+		</Location>
 </VirtualHost>
