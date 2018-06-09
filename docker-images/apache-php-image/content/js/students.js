b189b2ce107cf922942ce68e1a8dcf3c89f626fb
Elie Kin <elie.ndjoli@outlook.com>
2018-06-09 13:04:25 +0200

    maj copyright

--- docker-images/apache-php-image/content/js/students.js
+++ docker-images/apache-php-image/content/js/students.js
@@ -1,16 +1,24 @@
 $(function(){
         console.log("loading students");
 
+		//Fonction loadStudent
         function loadStudents(){
+		
+		//requête ajax récupérant le code JSON retourné par le serveur dynamique
         $.getJSON("/api/students/", function(students){
                 console.log(students);
                 var message = "Nobody is here";
                 if(students.length > 0){
+						//students[0].nom permet de récupérer le nom du premier student 
                         message = "welcome to HEIG-VD" + " " + students[0].nom;
                 }
+				//Le texte de message est affiché à partout où la classe navbar-brand est trouvé
                 $(".navbar-brand").text(message);
         });
      };
+	 //appel de la fonction loadStudents
      loadStudents();
+	 
+	 //rechargement de la fonction toute les 2 secondes
      setInterval(loadStudents, 2000);
 });
